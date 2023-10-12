0996fda44abd9d07818ebaf8a57b53bcdf7ed34e
Jasper <jasper@jazper.dk>
2023-10-12 05:03:01 +0200

    Renamed class to Address

--- revolut/models/address.py
+++ revolut/models/address.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 
 
-class ShippingAddress(BaseModel):
+class Address(BaseModel):
     street_line_1: Optional[str]
     street_line_2: Optional[str]
     region: Optional[str]
