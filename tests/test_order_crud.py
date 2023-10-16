4d1a3f871612d04a65a18fb90f829e9a78bc3f34
Jasper <jasper@jazper.dk>
2023-10-16 05:42:52 +0200

    Add order get function tests

--- tests/test_order_crud.py
+++ tests/test_order_crud.py
@@ -5,35 +5,38 @@ from os import getenv
 
 API_KEY = getenv("API_KEY")
 
-test_order_id = None
-test_order_created = None
+test_order_created: OrderModel | None = None
 
+CLIENT = Client(secret_key=API_KEY, environment=Environment.SANDBOX)
+ORDER_CLIENT = Order(CLIENT)
+
+LOOP = asyncio.get_event_loop()
 
 def test_order_create():
-    client = Client(secret_key=API_KEY, environment=Environment.SANDBOX)
-    order = Order(client)
     new_order = OrderModel(amount=100, currency=Currency.DKK, description="Test description of this order.")
-    order_created = order.create(new_order)
+    order_created = ORDER_CLIENT.create(new_order)
 
     assert order_created.id is not None
-    global test_order_id
     global test_order_created
-    test_order_id = order_created.id
     test_order_created = order_created
 
 
 def test_order_create_async():
-    client = Client(secret_key=API_KEY, environment=Environment.SANDBOX)
-    order = Order(client)
     new_order = OrderModel(amount=100, currency=Currency.DKK, description="Test description of this order.")
-    loop = asyncio.get_event_loop()
-    order_created = loop.run_until_complete(order.create_async(new_order))
+    order_created = LOOP.run_until_complete(ORDER_CLIENT.create_async(new_order))
 
     assert order_created.id is not None
 
 
 def test_order_get():
-    pass
+    order = ORDER_CLIENT.get(test_order_created.id)
+    assert order.checkout_url == test_order_created.checkout_url
+
+
+def test_order_get_async():
+    order = LOOP.run_until_complete(ORDER_CLIENT.get_async(test_order_created.id))
+    assert order.checkout_url == test_order_created.checkout_url
+
 
 
 def test_order_update():
