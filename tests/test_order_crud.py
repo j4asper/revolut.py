f18d25e9349f81dcf40d2ab139b7756d1ac16857
Jasper <jasper@jazper.dk>
2023-10-17 04:23:00 +0200

    Update typehint to fix tests on py v3.9

--- tests/test_order_crud.py
+++ tests/test_order_crud.py
@@ -1,16 +1,18 @@
 
 import asyncio
 from revolut import Client, Environment, Order, OrderModel, Currency
+from typing import Optional
 from os import getenv
 
 API_KEY = getenv("API_KEY")
 
-test_order_created: OrderModel | None = None
+test_order_created: Optional[OrderModel] = None
 
 CLIENT = Client(secret_key=API_KEY, environment=Environment.SANDBOX)
 ORDER_CLIENT = Order(CLIENT)
 
 LOOP = asyncio.get_event_loop()
+
 
 def test_order_create():
     new_order = OrderModel(amount=100, currency=Currency.DKK, description="Test description of this order.")
@@ -36,7 +38,6 @@ def test_order_get():
 def test_order_get_async():
     order = LOOP.run_until_complete(ORDER_CLIENT.get_async(test_order_created.id))
     assert order.checkout_url == test_order_created.checkout_url
-
 
 
 def test_order_update():
